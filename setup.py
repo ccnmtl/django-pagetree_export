f0c87670e288c3f504e5be199e098d721bfb70f9
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2011-06-20 12:18:30 -0400

    0.2 tag

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ from setuptools import setup
 
 setup(
     name="django-pagetree_export",
-    version="0.1",
+    version="0.2",
     author="Ethan Jucovy",
     author_email="ccnmtl-dev@ccnmtl.columbia.edu",
     url="",
